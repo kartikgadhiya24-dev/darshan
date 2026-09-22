45e2c454863228e73de21854a6576d122cea9878
kartikgadhiya24-dev <kartikgadhiya24@gmail.com>
2026-09-22 15:30:10 +0530

    Update shared/networking/src/lib.rs

--- shared/networking/src/lib.rs
+++ shared/networking/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod discovery;
 pub mod connection;
 pub mod nat;
+pub mod relay;
 
 pub use discovery::{DiscoveryService, DiscoveryBeacon};
 pub use connection::{DirectServer, DirectClient};
+pub use relay::{RelayClient, RelayPacket};
